6fd622099a72dab006749d7a9f8e40c6890e22d0
Tony Nguyen <tony.nguyen@epicosity.com>
2019-11-07 12:33:26 -0600

    add recaptcha to contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,11 +3,13 @@ import React from 'react';
 export default class ContactForm extends React.Component {
     render() {
         return (
-            <form name="contactForm" method="POST" netlifyHoneypot="bot-field" data-netlify="true" id="contact-form"
-              className="contact-form">
-              <p className="screen-reader-text">
-                <label>Don't fill this out if you're human: <input name="bot-field" /></label>
-              </p>
+            <form 
+              name="contactForm" method="POST" 
+              data-netlify-recaptcha="true" 
+              data-netlify="true" 
+              id="contact-form"
+              className="contact-form"
+            >
               <p className="form-row">
                 <label className="form-label" htmlFor="contact-user-name">Name</label>
                 <input type="text" name="name" id="contact-user-name" className="form-input" placeholder="Enter your name"/>
@@ -23,6 +25,7 @@ export default class ContactForm extends React.Component {
                 <textarea name="message" id="contact-message" className="form-textarea" rows="5" placeholder="Enter your message" />
                 <span className="input-focus" aria-hidden="true" />
               </p>
+              <div data-netlify-recaptcha="true"></div>
               <input type="hidden" name="form-name" value="contactForm" />
               <p className="form-row form-submit">
                 <button type="submit" className="button">Send Message</button>
